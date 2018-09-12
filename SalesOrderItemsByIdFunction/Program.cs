46087286345ca1f19b468fc17d0c6115d84c3426
Shalitha Suranga <shalithas@99x.lk>
2018-09-12 10:49:18 +0530

    Add project files.

---
+++ SalesOrderItemsByIdFunction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesOrderItemsByIdFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
